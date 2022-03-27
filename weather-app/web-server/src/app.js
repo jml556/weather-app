f60c1f8d26c113158c1c52fd69ac2cda50afbc84
jml556 <jmleesjc2000@gmail.com>
2022-03-27 16:44:33 -0400

    added templating

--- weather-app/web-server/src/app.js
+++ weather-app/web-server/src/app.js
@@ -1,34 +1,64 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+//Define paths for Express config
+const publicDirectory = path.join(__dirname, "..", "public");
+const templateDirectory = path.join(__dirname, "../../templates/views");
+const partialsDirectory = path.join(__dirname, "../../templates/partials");
 
-const html = `
-<h1>About Page using htnml</h1>
-`
+//setup handlebars engine and views
+app.set("view engine", "hbs");
+app.set("views", templateDirectory);
+hbs.registerPartials(partialsDirectory);
+//setup static directory to serve
+app.use(express.static(publicDirectory));
 
 const obj = {
-  location: 'Maryland',
-  name: 'Tim'
-}
-//about title
-app.get('/about', (req, res) => {
-  res.send(html)
-})
+  location: "Maryland",
+  name: "Tim",
+};
 
-app.get('/help', (req, res) => {
-  res.send('Help Page')
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "weather app",
+    name: "Joe",
+  });
+});
 
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Joe",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "help page",
+    name: "Joe",
+  });
+});
 
 //json response - send back object
-app.get('/weather', (req, res) => {
-  res.send(obj)
+app.get("/weather", (req, res) => {
+  res.send(obj);
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('notfound', {
+    text: "Article could not be found"
+  })
 })
 
+app.get('*', (req, res) => {
+  res.rednder('notfound', {
+    text: "Help article could not be found"
+  })
+});
+
 app.listen(3000, () => {
-  console.log('listening on PORT ' + 3000)
-})
+  console.log("listening on PORT " + 3000);
+});
